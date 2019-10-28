2890a42cef65b4f4bc1e37010d259fb957125d19
Nathan Lam <z5113345@student.unsw.edu.au>
2019-10-28 23:47:03 +1100

    Implementing identity

---
+++ credit-reporting/organization/lender/contract/lib/identity.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Utility class for ledger state
+const State = require('./../ledger-api/state.js');
+
+// Enumerate commercial paper state values
+const cpState = {
+    SUBMITTED: 1,
+    VERIFIED: 2,
+};
+
+class BorrowerIdentity extends State {
+
+    constructor(obj) {
+        super(CommercialPaper.getClass(), [obj.surname, obj.givenNames, obj.birthDate, obj.driverLicenseNo]);
+        Object.assign(this, obj);
+    }
+
+    setSubmitted() {
+        this.currentState = SUBMITTED;
+    }
+
+    setVerified() {
+        this.currentState = VERIFIED;
+    }
+
+    isSubmitted() {
+        return this.currentState === cpState.SUBMITTED;
+    }
+
+    isVerified() {
+        return this.currentState === cpState.VERIFIED;
+    }
+
+    static fromBuffer(buffer) {
+        return BorrowerIdentity.deserialize(buffer);
+    }
+
+    toBuffer() {
+        return Buffer.from(JSON.stringify(this));
+    }
+
+    /**
+     * Deserialize a state data to commercial paper
+     * @param {Buffer} data to form back into the object
+     */
+    static deserialize(data) {
+        return State.deserializeClass(data, BorrowerIdentity);
+    }
+
+    /**
+     * Factory method to create a commercial paper object
+     */
+    static createInstance(surname, givenNames, birthDate, currentAddress, previousAddress1, previousAddress2, latestEmployer, driverLicenseNo) {
+        return new CommercialPaper({surname, givenNames, birthDate, currentAddress, previousAddress1, previousAddress2, latestEmployer, driverLicenseNo });
+    }
+
+    static getClass() {
+        return 'org.creditnet.borroweridentity';
+    }
+}
+
+module.exports = BorrowerIdentity;
